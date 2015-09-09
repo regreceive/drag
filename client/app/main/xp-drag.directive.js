b7ce7d80d50e27e66f9656f6dea059646a339e4d
regreceive <regreceive@163.com>
2015-09-09 23:02:30 +0800

    shim placeholder in jqueryUI

--- client/app/main/xp-drag.directive.js
+++ client/app/main/xp-drag.directive.js
@@ -2,11 +2,23 @@ angular.module('dragApp')
   .directive('xpDrag', function() {
     return {
       link: function(scope, element, attrs) {
+        var dropSuccess = false;
         element.sortable({
           connectWith: '[xp-drop]',
-          revert: true,
+          revert: 100,
           over: function( event, ui ) {
             ui.placeholder.removeClass().addClass('col-md-6');
+          },
+          start: function( event, ui ) {
+            ui.placeholder.html('<div class="thumbnail"><img src="assets/images/shortcut_gis.png" /><div class="caption"><h6>&nbsp;</h6></div></div>');
+          },
+          stop: function( event, ui ) {
+            if (!dropSuccess) {
+              element.sortable('cancel');
+            }
+          },
+          remove: function( event, ui ) {
+            dropSuccess = true;
           }
         });
       }
--- client/app/main/xp-drop.directive.js
+++ client/app/main/xp-drop.directive.js
@@ -3,44 +3,47 @@ angular.module('dragApp')
     return {
       link: function(scope, element, attrs) {
         element.sortable({
+          revert: 100,
+          placeholder: 'sortable-placeholder',
           activate: function( event, ui ) {
-            console.log('active');
+            //console.log('active');
           },
           beforeStop: function( event, ui ) {
-            console.log('beforeStop')
+            //console.log('beforeStop')
           },
           change: function( event, ui ) {
-            console.log('change');
+            //console.log('change');
           },
           create: function( event, ui ) {
-            console.log('create');
+            //console.log('create');
           },
           deactivate: function( event, ui ) {
-            console.log('deactivate');
+            //console.log('deactivate');
           },
           out: function( event, ui ) {
-            console.log('out');
+            //console.log('out');
           },
           over: function( event, ui ) {
-            console.log('over');
-            ui.placeholder.removeClass().addClass('col-md-2 col-lg-2 col-sm-3');
+            //console.log('over');
+            ui.placeholder.removeClass().addClass('col-md-2 sortable-placeholder');
             ui.placeholder.html('<div class="thumbnail"><img src="assets/images/shortcut_gis.png" /><div class="caption"><h6>&nbsp;</h6></div></div>');
           },
           receive: function( event, ui ) {
-            console.log('receive');
-            ui.item.removeClass().addClass('col-md-2 col-lg-2 col-sm-3');
+            //console.log('receive');
+            ui.item.removeClass().addClass('col-md-2');
           },
           remove: function( event, ui ) {
-            console.log('remove');
+            //console.log('remove');
           },
           sort: function( event, ui ) {
-            console.log('sort');
+            //console.log('sort');
           },
           start: function( event, ui ) {
-            console.log('start');
+            //console.log('start', ui);
+            ui.placeholder.html('<div class="thumbnail"><img src="assets/images/shortcut_gis.png" /><div class="caption"><h6>&nbsp;</h6></div></div>');
           },
           stop: function( event, ui ) {
-            console.log('stop');
+            //console.log('stop');
           },
           update: function( event, ui ) {
             console.log('update');
